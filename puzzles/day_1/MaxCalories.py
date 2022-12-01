3046e3cf65f70d2668372007b8588b83cb8a1263
Emil Calonius <emil.calonius@tuni.fi>
2022-12-02 00:19:13 +0200

    add comments to day 1 solution

--- puzzles/day_1/MaxCalories.py
+++ puzzles/day_1/MaxCalories.py
@@ -1,6 +1,18 @@
+"""
+Elves are carrying snacks on their expedition. The amount of calories carried by each elf
+can be found in a text file. Each elf can carry multiple items and the elves' inventories are
+separated by an empty row.
+
+Part 1.
+Find the elf carrying most calories. 
+
+Part2.
+Find top three elves based on how many calories they are carrying
+"""
+
+# Read file and add each elf's total calories into a list
 calories_arr = []
 calories = 0
-
 with open("calories.txt", "r") as file:
     for line in file:
         line = line.replace("\n", "")
@@ -10,15 +22,16 @@ with open("calories.txt", "r") as file:
             continue
         calories += int(line)
 
+# Find elf with most total calories
 max_calories = max(calories_arr)
 print("Most calories carried: " + str(max_calories) + 
 " calories by elf #" + str(calories_arr.index(max_calories) + 1))
 
+# Find top n elves based on how many calories they are carrying
 temp_arr = calories_arr.copy()
 # Change n_of_elves according to how many of the elves with most calories you want to find
 n_of_elves = 3
 max_n_calories = []
-
 for n in range(0, n_of_elves, 1):
     max_calories = max(temp_arr)
     max_n_calories.append(max_calories)
